95ff7c4ad1d47669756980d71fffc2973943b923
Igor Gerbylev <gerbylev@gmail.com>
2023-01-31 15:36:54 +0300

    update client example

--- mlp-examples/mlp-client-app/src/main/kotlin/client/ClientSdkExample.kt
+++ mlp-examples/mlp-client-app/src/main/kotlin/client/ClientSdkExample.kt
@@ -7,28 +7,15 @@ import com.mlp.sdk.MlpClientSDK
 val objectMapper = ObjectMapper().apply {
     registerModule(KotlinModule())
 }
-const val ACCOUNT = "your_account"
-const val MODEL = "your_model"
 
 fun main() {
     val clientSDK = MlpClientSDK()
     clientSDK.init()
 
-    val dataToPredict = getDataToPredict()
-    val results = dataToPredict.map { data ->
-        val payload = objectMapper.writeValueAsString(ClientPayloadData(data))
-        clientSDK.predict(ACCOUNT, MODEL, payload)
-    }
+    val payload = objectMapper.writeValueAsString("hello")
+    val res = clientSDK.predict("just-ai", "test-action", payload)
 
-    /* process results */
+    println(res)
 
     clientSDK.shutdown()
 }
-
-fun getDataToPredict(): List<String> {
-    return listOf("hello", "goodbye")
-}
-
-data class ClientPayloadData(
-    val data: String
-)
