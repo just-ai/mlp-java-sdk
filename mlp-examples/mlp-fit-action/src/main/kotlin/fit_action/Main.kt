71b6df6e54e4eac8209fd8fd47dbf5ea85f7e18b
Igor Gerbylev <gerbylev@gmail.com>
2023-04-23 11:23:54 +0300

    fit action example for miltiFit and with new APIs

--- mlp-examples/mlp-fit-action/src/main/kotlin/fit_action/Main.kt
+++ mlp-examples/mlp-fit-action/src/main/kotlin/fit_action/Main.kt
@@ -1,119 +1,33 @@
 package fit_action
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.KotlinModule
-import com.mlp.gate.DatasetInfoProto
-import com.mlp.gate.ServiceDescriptorProto
-import com.mlp.gate.ServiceInfoProto
-import com.mlp.sdk.MlpResponse
-import com.mlp.sdk.MlpResponseException
-import com.mlp.sdk.MlpService
 import com.mlp.sdk.MlpServiceSDK
-import com.mlp.sdk.Payload
-import com.mlp.sdk.storage.StorageFactory
+import com.mlp.sdk.utils.JSON
+
+enum class Mode {
+    single,
+    multi
+}
+data class InitConfigData(val mode: Mode = Mode.single)
+data class FitDatasetData(val map: Map<String, String>)
+data class FitConfigData(val upper: Boolean)
+data class PredictRequestData(val text: String)
+data class PredictResponseData(val text: String)
 
 fun main() {
-    val action = FitTestAction()
-    val actionSDK = MlpServiceSDK(action)
-
-    actionSDK.start()
-    actionSDK.blockUntilShutdown()
-}
-
-class FitTestAction : MlpService() {
-
-    private val objectMapper = ObjectMapper().apply {
-        registerModule(KotlinModule())
-    }
-    private val storage = StorageFactory.getStorage()
-    private val defaultStorageDir = StorageFactory.getDefaultStorageDir()
-    private val modelFileName = "model_fit_result.json"
-
-    private var model: FittedModel? = loadState()
-
-    override fun getDescriptor(): ServiceDescriptorProto {
-        return ServiceDescriptorProto.newBuilder()
-            .setName("my-fit")
-            .setFittable(true)
-            .build()
-    }
-
-    override fun fit(
-        train: Payload,
-        targets: Payload?,
-        config: Payload?,
-        modelDir: String,
-        previousModelDir: String?,
-        targetServiceInfo: ServiceInfoProto,
-        dataset: DatasetInfoProto
-    ): MlpResponse {
-        val trainData = objectMapper.readValue(train.data, TransformerFitTrainData::class.java)
-            .texts
-        val targetData = objectMapper.readValue(targets!!.data, TransformerFitTargets::class.java)
-            .items_list
-            .map { it.items.first() }
-            .map { it.value }
-        if (trainData.size != targetData.size) {
-            return MlpResponseException(
-                RuntimeException("Inconsistent data sizes: ${trainData.size} texts and ${targetData.size} items lists")
-            )
+    val initConfig = JSON.parse(System.getenv().get("SERVICE_CONFIG") ?: """{"model":"single"}""", InitConfigData::class.java)
+    val service =
+        when (initConfig.mode) {
+            Mode.single -> SingleFit()
+            Mode.multi ->
+                if (System.getenv()["MLP_STORAGE_DIR"].isNullOrEmpty()) {
+                    FitService()
+                } else {
+                    PredictService()
+                }
         }
-        val result = processFitData(trainData, targetData)
-        val storageDir = modelDir.ifBlank { defaultStorageDir }
-        storage.saveState(objectMapper.writeValueAsString(result), "$storageDir/$modelFileName")
-        this.model = FittedModel(result)
 
-        return Payload(
-            dataType = "text/plain",
-            data = "ok"
-        )
-    }
+    val mlp = MlpServiceSDK(service)
 
-    override fun predict(req: Payload, config: Payload?): MlpResponse {
-        val requestData = objectMapper.readValue(req.data, PredictRequestData::class.java)
-        return requireNotNull(model).predict(requestData.number)
-    }
-
-    private fun loadState(): FittedModel? {
-        return try {
-            storage.loadState("$defaultStorageDir/$modelFileName")
-                ?.let { objectMapper.readValue(it, FitProcessData::class.java) }
-                ?.let { FittedModel(it) }
-        } catch (e: Exception) {
-            null
-        }
-    }
-
-    private fun processFitData(trainData: List<String>, targetData: List<String>): FitProcessData {
-        return targetData.mapIndexed { i, data ->
-            data to trainData[i]
-        }.toMap().let {
-            FitProcessData(it)
-        }
-    }
-}
-
-data class PredictRequestData(
-    val number: String
-)
-
-data class FitProcessData(val processedData: Map<String, String>)
-data class TransformerFitTrainData(val texts: List<String>)
-data class TransformerFitTargetItem(val value: String)
-data class TransformerFitTargetItems(val items: List<TransformerFitTargetItem>)
-data class TransformerFitTargets(
-    val items_list: List<TransformerFitTargetItems>,
-    val extra_items_list: List<TransformerFitTargetItems>
-)
-
-class FittedModel(
-    private val data: FitProcessData
-) {
-
-    fun predict(number: String): MlpResponse {
-        val value = data.processedData[number]
-            ?: return MlpResponseException(RuntimeException("No element found"))
-        return Payload(value)
-    }
-
+    mlp.start()
+    mlp.blockUntilShutdown()
 }
