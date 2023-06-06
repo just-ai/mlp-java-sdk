502a8a014399bcf7c8ef1244b6307ec13bfdfc2e
Igor Gerbylev <igor.gerbylev@just-ai.com>
2023-06-06 11:26:11 +0300

    dependencies rework

--- mlp-sdk/src/main/kotlin/com/mlp/sdk/storage/S3Storage.kt
+++ mlp-sdk/src/main/kotlin/com/mlp/sdk/storage/S3Storage.kt
@@ -3,7 +3,11 @@ package com.mlp.sdk.storage
 import io.minio.GetObjectArgs
 import io.minio.MinioClient
 import io.minio.PutObjectArgs
+import io.minio.errors.ErrorResponseException
 import org.apache.commons.io.IOUtils
+import java.io.ByteArrayInputStream
+import java.io.File
+import java.io.FileInputStream
 
 class S3Storage(
     val minioClient: MinioClient,
@@ -21,17 +25,52 @@ class S3Storage(
         }
     }
 
+    override fun saveState(content: ByteArray, filePath: String) {
+        with(ByteArrayInputStream(content)) {
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(bucketName)
+                            .`object`(filePath)
+                            .stream(this, this.available().toLong(), -1)
+                            .build()
+            )
+        }
+    }
+
+    override fun saveState(content: File, filePath: String) {
+        with(FileInputStream(content)) {
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(bucketName)
+                            .`object`(filePath)
+                            .stream(this, this.available().toLong(), -1)
+                            .build()
+            )
+        }
+    }
+
     override fun loadState(path: String): String? {
-        val modelResponseBytes = minioClient.getObject(
-            GetObjectArgs.builder()
-                .bucket(bucketName)
-                .`object`(path)
-                .build()
-        ).readAllBytes()
-        if (modelResponseBytes.isEmpty()) {
+        return loadStateBytes(path)?.let { String(it) }
+    }
+
+    override fun loadStateBytes(path: String): ByteArray? {
+        try {
+            val modelResponseBytes = minioClient.getObject(
+                    GetObjectArgs.builder()
+                            .bucket(bucketName)
+                            .`object`(path)
+                            .build()
+            ).readAllBytes()
+            if (modelResponseBytes.isEmpty()) {
+                return null
+            }
+            return modelResponseBytes
+        } catch (e: ErrorResponseException) {
+            if (e.errorResponse().code() != "NoSuchKey") {
+                throw e
+            }
             return null
         }
-        return String(modelResponseBytes)
     }
 
     companion object {
--- mlp-sdk/src/main/kotlin/com/mlp/sdk/storage/Storage.kt
+++ mlp-sdk/src/main/kotlin/com/mlp/sdk/storage/Storage.kt
@@ -1,6 +1,12 @@
 package com.mlp.sdk.storage
 
+import java.io.File
+
 interface Storage {
     fun saveState(content: String, filePath: String)
+    fun saveState(content: ByteArray, filePath: String)
+    fun saveState(content: File, filePath: String)
+
     fun loadState(path: String): String?
+    fun loadStateBytes(path: String): ByteArray?
 }
