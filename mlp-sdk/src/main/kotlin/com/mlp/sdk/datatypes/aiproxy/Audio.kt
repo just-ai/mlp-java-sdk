5c647c718d9586acb5b100e88a501b078887a252
Vitalii Protasov <v.protasov@just-ai.com>
2023-08-01 23:51:05 +0300

    Add some types of asr

--- mlp-sdk/src/main/kotlin/com/mlp/sdk/datatypes/aiproxy/Audio.kt
+++ mlp-sdk/src/main/kotlin/com/mlp/sdk/datatypes/aiproxy/Audio.kt
@@ -3,6 +3,7 @@ package com.mlp.sdk.datatypes.aiproxy
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.web.multipart.MultipartFile
+import java.time.Duration
 
 
 /**
@@ -19,7 +20,7 @@ data class CreateTranscriptionRequest(
     @JsonProperty("response_format")
     val responseFormat: String? = null,
 
-    val temperature: Int? = null,
+    val temperature: Double? = null,
 
     val language: String? = null,
 )
@@ -38,10 +39,29 @@ data class CreateTranslationRequest(
     @JsonProperty("response_format")
     val responseFormat: String? = null,
 
-    val temperature: Int? = null
+    val temperature: Double? = null
 )
-
 
 data class AudioSubtitlesResult(
     val text: String,
+    val task: String? = null,
+    val language: String? = null,
+    val duration: Duration? = null,
+    val segments: List<AudioSegment>? = null
+)
+
+data class AudioSegment(
+    val id: Long? = null,
+    val seek: Long? = null,
+    val start: Double? = null,
+    val end: Double? = null,
+    val text: String? = null,
+    val tokens: List<Int>? = null,
+    val temperature: Double? = null,
+    @JsonProperty("avg_logprob")
+    val avgLogProb: Double? = null,
+    @JsonProperty("compression_ratio")
+    val compressionRatio: Double? = null,
+    @JsonProperty("no_speech_prob")
+    val noSpeechProb: Double? = null,
 )
