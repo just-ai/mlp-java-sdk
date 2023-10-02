eb836fc226b72fbd29bb30e6b76e87e6aeff8624
Protasov Vitalii <v.protasov@just-ai.com>
2023-10-02 16:00:42 +0300

    CAILA-2033: WIP

--- mlp-sdk/src/main/kotlin/com/mlp/sdk/utils/WithLogger.kt
+++ mlp-sdk/src/main/kotlin/com/mlp/sdk/utils/WithLogger.kt
@@ -1,19 +1,11 @@
 package com.mlp.sdk.utils
 
-import org.slf4j.ILoggerFactory
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 @Deprecated("Use logger of MlpExecutionContext")
 interface WithLogger {
 
-    /**
-     * You can specify your factory implementation to define your loggers
-     */
-    val loggerFactory: ILoggerFactory?
-        get() = null
-
     val logger: Logger
-        get() = loggerFactory?.getLogger(javaClass.name)
-            ?: LoggerFactory.getLogger(javaClass)
+        get() = LoggerFactory.getLogger(javaClass)
 }
