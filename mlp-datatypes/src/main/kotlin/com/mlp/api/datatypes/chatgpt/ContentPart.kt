a8d44929f2c4cc87f47dd005f367ba7b8f58081c
vitekkor <v.korotkikh@just-ai.com>
2024-07-03 23:12:48 +0300

    CAILA-2997: remove .openapi-generator-ignore

--- mlp-datatypes/src/main/kotlin/com/mlp/api/datatypes/chatgpt/ContentPart.kt
+++ mlp-datatypes/src/main/kotlin/com/mlp/api/datatypes/chatgpt/ContentPart.kt
@@ -1,21 +1,27 @@
 package com.mlp.api.datatypes.chatgpt
 
+import java.util.Objects
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.annotation.JsonValue
+import com.mlp.api.datatypes.chatgpt.ContentPartType
 import io.swagger.v3.oas.annotations.media.Schema
 
 /**
- *
- * @param type
+ * 
+ * @param type 
  */
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type", visible = true)
 @JsonSubTypes(
-    JsonSubTypes.Type(value = ImageContentPart::class, name = "image_url"),
-    JsonSubTypes.Type(value = TextContentPart::class, name = "text")
+      JsonSubTypes.Type(value = ImageContentPart::class, name = "image_url"),
+      JsonSubTypes.Type(value = TextContentPart::class, name = "text")
 )
-interface ContentPart {
-    @get:Schema(example = "null")
+
+interface ContentPart{
+            @get:Schema(example = "null", required = true   , description = "")
     val type: ContentPartType
+
 }
 
