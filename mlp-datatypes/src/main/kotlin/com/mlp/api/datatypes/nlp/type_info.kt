dd143cea85cb2ef1b22e44f10a8a61fd5051c30e
Igor Gerbylev <igor.gerbylev@just-ai.com>
2024-08-20 10:58:51 +0300

    add nlp data-scheme

---
+++ mlp-datatypes/src/main/kotlin/com/mlp/api/datatypes/nlp/type_info.kt
@@ -0,0 +1,9 @@
+package com.mlp.api.datatypes.nlp
+
+import com.mlp.api.TypeInfoBase
+
+object type_info: TypeInfoBase {
+
+    override val canonicalUrl = "https://caila.io/specs/mlp-data-nlp.yml"
+
+}
